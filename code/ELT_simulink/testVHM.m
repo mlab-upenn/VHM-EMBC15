3ae4124afae14071caf66b2b9681679665132a8b
houssamya <habbas@seas.upenn.edu>
2015-04-05 16:04:42 -0400

    changed to temp rob

--- code/ELT_simulink/testVHM.m
+++ code/ELT_simulink/testVHM.m
@@ -69,71 +69,80 @@ nb_pulses = cp_array/2
 % VS is given by NA3_Out
 disp(' ')
 disp('The specification:')
-% phi = '(VP \/ PVC \/ VS) -> ([]_[0,500] !VP)'
-phi = 'VP'
+phi = '[](vp \/ vs \/ pvc) -> ([]_[0,500] !vp)'
+% More accurate spec: on falling edge of any of the signals, count 500 ms of no VP
+phi_vp  = '((vp /\ X(!vp))   -> X([]_[0,500]!vp))';
+phi_vs  = '((vs /\ X(!vs))   -> X([]_[0,500]!vp))';
+phi_pvc = '((pvc /\ X(!pvc)) -> X([]_[0,500]!vp))';
 
-preds(1).str='VP';
+phifull = ['[] ( ', phi_vp,' /\', phi_vs, '/\', phi_pvc, ' )'];
+%phifull = '[]( ((vp /\ X(!vp)) -> X([]_[0,500]!vp)) /\ ((vs /\ X(!vs)) -> X([]_[0,500]!vp)) /\ ((pvc /\ X(!pvc)) -> X([]_[0,500]!vp)) )';
+
+preds(1).str='vp';
 preds(1).A = [zeros(1,7), -1, 0];
-preds(1).b = -1;
+preds(1).b = -0.8;
 
-preds(2).str='PVC';
+preds(2).str='pvc';
 preds(2).A = [zeros(1,8), -1];
-preds(2).b = -1;
+preds(2).b = -0.8;
 
-preds(3).str='VS';
+preds(3).str='vs';
 preds(3).A = [0, 0, -1, zeros(1,6)];
-preds(3).b = -1;
+preds(3).b = -0.8;
 
 disp(' ')
 disp('Total Simulation time:')
-simTime = 5
+simTime = 5000
 
 opt = staliro_options();
 
-opt.optimization_solver = 'UR_Taliro';
-opt.runs = 2;
-opt.ur_params.n_tests = 3;
+opt.optimization_solver = 'SA_Taliro';
+opt.runs = 3;
+opt.sa_params.n_tests = 3;%1000;
 opt.spec_space='Y';
 opt.interpolationtype = {'bool'};
 opt.n_workers = 1;
+opt.varying_cp_times = 1;
+opt.taliro = 'dp_t_taliro';
 
 open_system(sprintf('%s/pacemaker_DDD/pacem_aut',model));
 
 disp(' ')
-disp('Running S-TaLiRo with Uniform Random Sampling ...')
 tic
 results = staliro(model,init_cond,input_range,cp_array,phi,preds,simTime,opt);
 toc
+save('results.mat')
 
 disp(' ')
 display(['Minimum Robustness found in Run 1 = ',num2str(results.run(1).bestRob)])
 display(['Minimum Robustness found in Run 2 = ',num2str(results.run(2).bestRob)])
 
-disp(' ')
-disp('Displaying trajectories in Figures 1 and 2 ...')
-
+[T1,XT1,YT1,IT1] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(1).bestSample,simTime,opt);
+VS1 = YT1(:,3);
+VP1 = YT1(:,8);
+PVC1 = YT1(:,9);
 figure(1)
 clf
-[T1,XT1,YT1,IT1] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(1).bestSample,simTime,opt);
 subplot(2,1,1)
-plot(T1,XT1)
+plot(T1,YT1(:,[3,8,9]))
 title('State trajectories')
+legend('VS_3', 'VP', 'PVC')
 subplot(2,1,2)
-plot(IT1(:,1),IT1(:,2))
+plot(IT1(:,1),IT1(:,3))
 title('Input Signal')
 
+[T2,XT2,YT2,IT2] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(2).bestSample,simTime,opt);
+VS2 = YT2(:,3);
+VP2 = YT2(:,8);
+PVC2 = YT2(:,9);
 figure(2)
 clf
-[T2,XT2,YT2,IT2] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(2).bestSample,simTime,opt);
 subplot(2,1,1)
-plot(T2,XT2)
+plot(T2,YT2(:,[3,8,9]))
 title('State trajectories')
+legend('VS_3', 'VP', 'PVC')
 subplot(2,1,2)
-plot(IT2(:,1),IT2(:,2))
+plot(IT2(:,1),IT2(:,3))
 title('Input Signal')
-
-disp(' ')
-disp('Displaying Simulink model for visualization. ')
-open(model)
 
 
