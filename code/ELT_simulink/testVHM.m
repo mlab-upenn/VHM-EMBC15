f4398c90d1b7a562a96564876c59f37568699e2b
houssamya <habbas@seas.upenn.edu>
2015-04-05 19:50:20 -0400

    stashing before pull

--- code/ELT_simulink/testVHM.m
+++ code/ELT_simulink/testVHM.m
@@ -62,22 +62,21 @@ disp('The constraints on the input signals PVC and PAC defined as a hypercube:')
 input_range = [0 1; 0 1]
 disp(' ')
 disp('The number of control points for the input signal determines how many pulses there are:')
-cp_array = [10, 10];
+cp_array = [15, 15];
 nb_pulses = cp_array/2
 
 % output = NA1 ... NA7, VP, PVC
 % VS is given by NA3_Out
 disp(' ')
 disp('The specification:')
-phi = '[](vp \/ vs \/ pvc) -> ([]_[0,500] !vp)'
+phi = '[](vp \/ vs \/ pvc ) -> ([]_[1,500] !vp)'
 % More accurate spec: on falling edge of any of the signals, count 500 ms of no VP
-phi_vp  = '((vp /\ X(!vp))   -> X([]_[0,500]!vp))';
-phi_vs  = '((vs /\ X(!vs))   -> X([]_[0,500]!vp))';
-phi_pvc = '((pvc /\ X(!pvc)) -> X([]_[0,500]!vp))';
+phi_vp  = '((vp /\ X(!vp)) -> X([]_[1,500]!vp))';
+phi_vs  = '((vs /\ X(!vs)) -> X([]_[1,500]!vp))';
+phi_pvc = '((pvc /\ X(!pvc)) -> X([]_[1,500]!vp))';
 
-phifull = ['[] ( ', phi_vp,' /\', phi_vs, '/\', phi_pvc, ' )'];
+phifull = ['[] ( ', phi_vp,' /\ ', phi_vs, ' /\ ', phi_pvc, ' )'];
 %phifull = '[]( ((vp /\ X(!vp)) -> X([]_[0,500]!vp)) /\ ((vs /\ X(!vs)) -> X([]_[0,500]!vp)) /\ ((pvc /\ X(!pvc)) -> X([]_[0,500]!vp)) )';
-
 preds(1).str='vp';
 preds(1).A = [zeros(1,7), -1, 0];
 preds(1).b = -0.8;
@@ -98,18 +97,20 @@ opt = staliro_options();
 
 opt.optimization_solver = 'SA_Taliro';
 opt.runs = 3;
-opt.sa_params.n_tests = 3;%1000;
+opt.sa_params.n_tests = 2;
 opt.spec_space='Y';
 opt.interpolationtype = {'bool'};
 opt.n_workers = 1;
 opt.varying_cp_times = 1;
 opt.taliro = 'dp_t_taliro';
 
-open_system(sprintf('%s/pacemaker_DDD/pacem_aut',model));
+%load_system(sprintf('%s.mdl',model));
+%load_system(sprintf('%s/pacemaker_DDD/pacem_aut',model));
+%set_param(sys_name,'StopTime','5000');
 
 disp(' ')
 tic
-results = staliro(model,init_cond,input_range,cp_array,phi,preds,simTime,opt);
+results = staliro(model,init_cond,input_range,cp_array,phifull,preds,simTime,opt);
 toc
 save('results.mat')
 
@@ -117,32 +118,5 @@ disp(' ')
 display(['Minimum Robustness found in Run 1 = ',num2str(results.run(1).bestRob)])
 display(['Minimum Robustness found in Run 2 = ',num2str(results.run(2).bestRob)])
 
-[T1,XT1,YT1,IT1] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(1).bestSample,simTime,opt);
-VS1 = YT1(:,3);
-VP1 = YT1(:,8);
-PVC1 = YT1(:,9);
-figure(1)
-clf
-subplot(2,1,1)
-plot(T1,YT1(:,[3,8,9]))
-title('State trajectories')
-legend('VS_3', 'VP', 'PVC')
-subplot(2,1,2)
-plot(IT1(:,1),IT1(:,3))
-title('Input Signal')
-
-[T2,XT2,YT2,IT2] = SimSimulinkMdl(model,init_cond,input_range,cp_array,results.run(2).bestSample,simTime,opt);
-VS2 = YT2(:,3);
-VP2 = YT2(:,8);
-PVC2 = YT2(:,9);
-figure(2)
-clf
-subplot(2,1,1)
-plot(T2,YT2(:,[3,8,9]))
-title('State trajectories')
-legend('VS_3', 'VP', 'PVC')
-subplot(2,1,2)
-plot(IT2(:,1),IT2(:,3))
-title('Input Signal')
 
 
